114307c46d467dd866c84ba76bce7708a4be1a1d
Mason <mason.smith@i4-insight.com>
2020-05-05 17:05:53 -0600

    IMplemented redux-logger for debugging

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,35 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {
+  configureStore,
+  getDefaultMiddleware,
+  ThunkAction,
+  Action,
+} from '@reduxjs/toolkit';
+
+// Local Dependencies
+import reducer from './rootReducer';
+
+// Create an array of middlewares
+let middleware = [...getDefaultMiddleware()] as const;
+// We don't want our logger middleware to be
+// visible in production environments
+if (process.env.NODE_ENV !== 'production') {
+  // Prevents importing redux-logger in prod
+  // thus reducing bundle size
+  const { createLogger } = require('redux-logger');
+  // Custom logger configuration
+  const logger = createLogger({
+    collapsed: true,
+    duration: true,
+    timestamp: false,
+  });
+  // Add the logger to middleware array
+  middleware = [...middleware, logger] as const;
+}
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer,
+  middleware,
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
